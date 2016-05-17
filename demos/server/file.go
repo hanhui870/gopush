fa5ce6dbf7e900255dbab1fd8fea69f8c4768c6c
Bruce <bruce@haimi.com>
2016-05-17 13:39:06 +0800

    demo server test

--- demos/server/file.go
+++ demos/server/file.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"html"
+	"log"
 )
 
 func main() {
 	// To serve a directory on disk (/tmp) under an alternate URL
 	// path (/tmpfiles/), use StripPrefix to modify the request
 	// URL's path before the FileServer sees it:
-	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("./runtime"))))
+	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("./runtime/log"))))
+
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Println("Server started...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 
