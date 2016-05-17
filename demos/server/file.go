20898361a992e1fe98fdd3b19441f6e321975000
Bruce <bruce@haimi.com>
2016-05-17 14:32:15 +0800

    param test

--- demos/server/file.go
+++ demos/server/file.go
@@ -14,7 +14,20 @@ func main() {
 	http.Handle("/tmpfiles/", http.StripPrefix("/tmpfiles/", http.FileServer(http.Dir("./runtime/log"))))
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		r.ParseForm()
+		r.ParseMultipartForm(16 * 1024 * 1024)
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path), r)
+		fmt.Fprintf(w, fmt.Sprintln("Request method:", r.Method, " header:", r.Header, " Params:", r.Form, " PostParams:", r.PostForm, " Host:", r.Host, " Remote:", r.RemoteAddr))
+
+		if f, ok := r.Form["f"]; ok {
+			for key, value := range f {
+				fmt.Fprintln(w, " param f value:", key, value)
+			}
+		}else {
+			fmt.Fprintln(w, " param f needed.")
+		}
+
+
 	})
 
 	log.Println("Server started...")
